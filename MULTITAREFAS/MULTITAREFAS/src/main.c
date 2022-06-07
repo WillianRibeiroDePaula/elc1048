91b0042f4dcb5a890edb40c763456d435dd53744
WillianRibeiroDePaula <105529016+WillianRibeiroDePaula@users.noreply.github.com>
2022-06-07 17:06:37 -0300

    implementação polled led

--- MULTITAREFAS/MULTITAREFAS/src/main.c
+++ MULTITAREFAS/MULTITAREFAS/src/main.c
@@ -1,219 +1,37 @@
-/**
- * \file
- *
- * \brief Exemplos diversos de tarefas e funcionalidades de um sistema operacional multitarefas.
- *
- */
-
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Este arquivo contem exemplos diversos de tarefas e 
- * funcionalidades de um sistema operacional multitarefas.
- *
- *
- * \par Conteudo
- *
- * -# Inclui funcoes do sistema multitarefas (atraves de multitarefas.h)
- * -# Inicizalizao do processador e do sistema multitarefas
- * -# Criacao de tarefas de demonstracao
- *
- */
-
-/*
- * Inclusao de arquivos de cabecalhos
- */
 #include <asf.h>
 #include "stdint.h"
 #include "multitarefas.h"
 
-/*
- * Prototipos das tarefas
- */
-void tarefa_1(void);
-void tarefa_2(void);
-void tarefa_3(void);
-void tarefa_4(void);
-void tarefa_5(void);
-void tarefa_6(void);
-void tarefa_7(void);
-void tarefa_8(void);
 
 
+void configure_extint_channel(void)
+{
+	struct extint_chan_conf config_extint_chan;
+	extint_chan_get_config_defaults(&config_extint_chan);
+	config_extint_chan.gpio_pin = BUTTON_0_EIC_PIN;
+	config_extint_chan.gpio_pin_mux = BUTTON_0_EIC_MUX;
+	config_extint_chan.gpio_pin_pull = EXTINT_PULL_UP;
+	config_extint_chan.detection_criteria = EXTINT_DETECT_BOTH;
+	extint_chan_set_config(BUTTON_0_EIC_LINE, &config_extint_chan);
+}
 
-
-/*
- * Configuracao dos tamanhos das pilhas
- */
-#define TAM_PILHA_1			(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_2			(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_3			(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_4			(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_5			(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_6			(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_7			(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_8			(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
-
-/*
- * Declaracao das pilhas das tarefas
- */
-uint32_t PILHA_TAREFA_1[TAM_PILHA_1];
-uint32_t PILHA_TAREFA_2[TAM_PILHA_2];
-uint32_t PILHA_TAREFA_3[TAM_PILHA_3];
-uint32_t PILHA_TAREFA_4[TAM_PILHA_4];
-uint32_t PILHA_TAREFA_5[TAM_PILHA_5];
-uint32_t PILHA_TAREFA_6[TAM_PILHA_6];
-uint32_t PILHA_TAREFA_7[TAM_PILHA_7];
-uint32_t PILHA_TAREFA_8[TAM_PILHA_8];
-uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
-
-/*
- * Funcao principal de entrada do sistema
- */
 int main(void)
 {
 	system_init();
+	configure_extint_channel();
 	
-	/* Criacao das tarefas */
-	/* Parametros: ponteiro, nome, ponteiro da pilha, tamanho da pilha, prioridade da tarefa */
-	
-	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 5);
-	
-	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 4);
-	
-	CriaTarefa(tarefa_3, "Tarefa 3", PILHA_TAREFA_3, TAM_PILHA_3, 3);
-	
-	CriaTarefa(tarefa_4, "Tarefa 4", PILHA_TAREFA_4, TAM_PILHA_4, 2);
-	
-	CriaTarefa(tarefa_5, "Tarefa 5", PILHA_TAREFA_5, TAM_PILHA_5, 1);
-	
-	CriaTarefa(tarefa_6, "Tarefa 6", PILHA_TAREFA_6, TAM_PILHA_6, 6);
-	
-		
-	
-		
-	/* Cria tarefa ociosa do sistema */
-	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
-	
-	/* Configura marca de tempo */
-	ConfiguraMarcaTempo();   
-	
-	/* Inicia sistema multitarefas */
-;	IniciaMultitarefas();
-	
-	/* Nunca chega aqui */
-	while (1)
-	{
-	}
-}
+	while (true) {
+		if (extint_chan_is_detected(BUTTON_0_EIC_LINE)) {
+			// Do something in response to EXTINT edge detection
+			bool button_pin_state = port_pin_get_input_level(BUTTON_0_PIN);
+			port_pin_set_output_level(LED_0_PIN, button_pin_state);
+			extint_chan_clear_detected(BUTTON_0_EIC_LINE);
+		}
 
-/* Tarefas de exemplo que usam funcoes para suspender/continuar as tarefas */
- uint32_t cont_1 = 0;
-/* Tarefas de exemplo que usam funcoes para suspender/continuar as tarefas */
-void tarefa_1(void)
-{
-	 
-	
-	for (;;)
-	{
-		cont_1++;
-		TarefaSuspende(1);
-		
-	   
-	   
-	}
-}
-
-uint32_t cont_2 = 0;
-
-void tarefa_2(void)
-{
-	 
-	 
-	 for (;;)
-	 {
-		 cont_2++;
-		 
-		 TarefaContinua(1);
-		 TarefaSuspende(2);
-		 
-		 
-		 
-		 
-	 }
-}
-
-uint32_t cont_3 = 0;
-
-void tarefa_3(void)
-{
-
-	 
-	 for (;;)
-	 {
-		 cont_3++;
-	    TarefaContinua(2);
-		TarefaSuspende(3);
-		
-		 
-	 }
-}
-
-
-uint32_t cont_4 = 0;
-
-void tarefa_4(void)
-{
-
-	
-	for (;;)
-	{
-		cont_4++;
-		TarefaContinua(3);
-		TarefaSuspende(4);
 		
 		
-	}
 }
-
-
-uint32_t cont_5 = 0;
-
-void tarefa_5(void)
-{
-	
-for (;;)
-{
-	cont_5++;
-	TarefaContinua(4);
-	
-	
+return 0;
 }
-	
-}
-
-
-
-void tarefa_6(void)
-{
-
-static uint32_t total = 0;
-
-for (;;)
-{
-	TarefaSuspende(1);
-	TarefaSuspende(2);
-	TarefaSuspende(3);
-	TarefaSuspende(4);
-	TarefaEspera(1000);
-	total = cont_1 + cont_2 + cont_3 + cont_4 + cont_5; 
-	
-}
-	
-}
-
 
 
