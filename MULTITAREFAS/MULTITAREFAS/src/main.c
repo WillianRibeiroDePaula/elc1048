38b5bffa4edd1248efcab26e6b48117413359fa1
WillianRibeiroDePaula <105529016+WillianRibeiroDePaula@users.noreply.github.com>
2022-06-07 17:21:00 -0300

    callback led

--- MULTITAREFAS/MULTITAREFAS/src/main.c
+++ MULTITAREFAS/MULTITAREFAS/src/main.c
@@ -2,7 +2,9 @@
 #include "stdint.h"
 #include "multitarefas.h"
 
-
+void configure_extint_channel(void);
+void configure_extint_callbacks(void);
+void extint_detection_callback(void);
 
 void configure_extint_channel(void)
 {
@@ -13,25 +15,36 @@ void configure_extint_channel(void)
 	config_extint_chan.gpio_pin_pull = EXTINT_PULL_UP;
 	config_extint_chan.detection_criteria = EXTINT_DETECT_BOTH;
 	extint_chan_set_config(BUTTON_0_EIC_LINE, &config_extint_chan);
-}
+}
+void configure_extint_callbacks(void)
+{
+	extint_register_callback(extint_detection_callback,
+	BUTTON_0_EIC_LINE,
+	EXTINT_CALLBACK_TYPE_DETECT);
+	extint_chan_enable_callback(BUTTON_0_EIC_LINE,
+	EXTINT_CALLBACK_TYPE_DETECT);
+}
+void extint_detection_callback(void)
+{
+	bool pin_state = port_pin_get_input_level(BUTTON_0_PIN);	port_pin_set_output_level(LED_0_PIN, pin_state);
+ }
 
 int main(void)
 {
+	
+	
 	system_init();
 	configure_extint_channel();
+	configure_extint_callbacks();
+	system_interrupt_enable_global();
 	
 	while (true) {
-		if (extint_chan_is_detected(BUTTON_0_EIC_LINE)) {
-			// Do something in response to EXTINT edge detection
-			bool button_pin_state = port_pin_get_input_level(BUTTON_0_PIN);
-			port_pin_set_output_level(LED_0_PIN, button_pin_state);
-			extint_chan_clear_detected(BUTTON_0_EIC_LINE);
-		}
+		/* Do nothing - EXTINT will fire callback asynchronously */
+	}
 
-		
-		
-}
-return 0;
+				
+
+	return 0;
 }
 
 
